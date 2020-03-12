cf4cfc5a9468ef758c7e9ed8e76df6b2d324d790
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-12 20:18:04 +0200

    feat(head) add <head> code, image assets

--- site/web/app/themes/fiipregatit/resources/views/partials/head.blade.php
+++ site/web/app/themes/fiipregatit/resources/views/partials/head.blade.php
@@ -2,5 +2,11 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="icon" type="image/png" href="@asset('images/fiipregatit_favicon_32x32.png')" />
+  <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700|Roboto:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.12.1/css/all.css" integrity="sha384-TxKWSXbsweFt0o2WqfkfJRRNVaPdzXJ/YLqgStggBVRREXkwU7OKz+xXtqOU4u8+" crossorigin="anonymous">
+
   @php wp_head() @endphp
 </head>
