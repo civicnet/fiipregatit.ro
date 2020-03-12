03d312dda7d3d7f683cffccca170707a8c43e824
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-13 01:45:16 +0200

    feat(survival-kit) add survival-kit section

--- site/web/app/themes/fiipregatit/resources/views/page-homepage.blade.php
+++ site/web/app/themes/fiipregatit/resources/views/page-homepage.blade.php
@@ -6,4 +6,5 @@
   @include('partials.guide-section')
   @include('partials.homepage-counter',
   ['guide_count' => 1, 'video_count' => 2, 'image_count' => 3])
+  @include('partials.survival-kit')
 @endsection
---
+++ site/web/app/themes/fiipregatit/resources/views/partials/survival-kit.blade.php
@@ -0,0 +1,42 @@
+ <div class="container-fluid" id="plan-personal-section">
+    <div class="container">
+      <h2>Trusă supraviețuire</h2>
+      <div class="row align-items-center trusa-row">
+        <div class="col d-none d-sm-block img-container">
+          <a href="/plan-personal">
+            <img
+              src="@asset('images/fiipregatit_planpersonal_colaj_v1.png')"
+              class="img-fluid"
+              alt="Trsuă de supraviețuire"/>
+          </a>
+        </div>
+        <div class="col">
+          <div class="headline">
+            Într-o situație de urgență reacționezi corect dacă ești
+            <span class="big-red">informat, pregătit, solidar</span>.
+          </div>
+          <div>
+            Planul personal pentru situații de urgență este compus în principal
+            din 3 elemente de bază:
+          </div>
+          <div class="element">
+            Să fii informat și pregătit înainte de situație
+          </div>
+          <div class="element">
+            Să știi ce trebuie să faci în timpul unei situații
+          </div>
+          <div class="element">
+            Să ai pregătit rucsacul cu cele necesare supraviețuirii
+          </div>
+        </div>
+      </div>
+
+      <div class="text-center">
+        <a rel="button" class="btn btn-secondary all-button" href="/plan-personal">
+          Vezi detaliile trusei
+          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </a>
+      </div>
+
+    </div>
+  </div>
