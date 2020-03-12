7628a26097721eb2cef682774391213254d019d2
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-12 07:02:40 +0200

    fix(theme) add theme setup code

---
+++ site/web/app/themes/fiipregatit/app/Config/Constants.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Config;
+
+final class Constants {
+    private function __construct() {}
+
+    const POST_TYPE_GUIDE = 'ghid';
+    const POST_TYPE_CAMPAIGN = 'campanie';
+    const POST_TYPE_LINK = 'link_util';
+
+    const HOMEPAGE_CAMPAIGNS_COUNT = 4;
+    const HOMEPAGE_GUIDE_COUNT = 7;
+
+    const LINK_COUNT = 12;
+
+    const GUIDE_METABOX_GALLERY = 'fii_pregatit_galerie';
+    const CAMPAIGN_METABOX_ATTACHMENTS = 'fii_pregatit_atasamente';
+    const CAMPAIGN_METABOX_VIDEO_GROUP = 'fii_pregatit_video_list';
+    const ABOUT_PAGE_METABOX_PARTNERS = 'fii_pregatit_parteneri';
+    const ABOUT_PAGE_METABOX_PARTNER_DESC = 'fii_pregatit_descriere_parteneri';
+
+    const PAGE_PERSONAL_PLAN = 'plan-personal';
+    const PAGE_FIRST_AID = 'prim-ajutor';
+    const PAGE_ABOUT = 'despre';
+
+    const PAGE_GHIDURI = 'ghiduri';
+    const PAGE_CAMPANII = 'campanii';
+  }
--- site/web/app/themes/fiipregatit/app/setup.php
+++ site/web/app/themes/fiipregatit/app/setup.php
@@ -23,6 +23,205 @@ add_action('wp_enqueue_scripts', function () {
  * Theme setup
  */
 add_action('after_setup_theme', function () {
+    register_post_type(
+        Config\Constants::POST_TYPE_CAMPAIGN,
+        array(
+            'labels' => array(
+                'name' => __('Campanii'),
+                'singular_name' => __('Campanie')
+            ),
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'public' => true,
+            'has_archive' => false,
+            'menu_icon' => 'dashicons-megaphone'
+        )
+    );
+
+    register_post_type(
+        Config\Constants::POST_TYPE_GUIDE,
+        array(
+            'labels' => array(
+                'name' => __('Ghiduri'),
+                'singular_name' => __('Ghid')
+            ),
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'public' => true,
+            'has_archive' => false,
+            'menu_icon' => 'dashicons-clipboard'
+        )
+    );
+
+    register_post_type(
+        Config\Constants::POST_TYPE_LINK,
+        array(
+            'labels' => array(
+                'name' => __('Linkuri utile'),
+                'singular_name' => __('Link')
+            ),
+            'public' => true,
+            'has_archive' => false,
+            'menu_icon' => 'dashicons-external'
+        )
+    );
+});
+
+add_action('cmb2_admin_init', function () {
+    // Custom Guides meta box
+    $cmb_guide = new_cmb2_box(array(
+        'id'            => 'galerie_foto_ghiduri',
+        'title'         => __('Galerie Foto', 'cmb2'),
+        'object_types'  => array(Config\Constants::POST_TYPE_GUIDE),
+        'context'       => 'side',
+        'show_names'    => true
+    ));
+
+    $cmb_guide->add_field(array(
+        'name' => '',
+        'desc' => 'Alege imaginile pe care dorești să le afișezi pentru ghidul acesta',
+        'id'   => Config\Constants::GUIDE_METABOX_GALLERY,
+        'type' => 'file_list',
+        'query_args' => array('type' => 'image'),
+        'text' => array(
+            'add_upload_files_text' => 'Adaugă imagini',
+            'remove_image_text' => 'Șterge imagine',
+            'file_text' => 'Imagine:',
+            'file_download_text' => 'Downloadează',
+            'remove_text' => 'Șterge',
+        ),
+    ));
+
+    // Custom Campaign meta box
+    $cmb_campaign = new_cmb2_box(array(
+        'id'            => 'attachments_campaign',
+        'title'         => __('Materiale de Informare', 'cmb2'),
+        'object_types'  => array(Config\Constants::POST_TYPE_CAMPAIGN),
+        //'context'       => 'side',
+        'show_names'    => true
+    ));
+
+    $cmb_campaign->add_field(array(
+        'name' => '',
+        'desc' => 'Alege materialele de informare / promo, pentru campania aceasta',
+        'id'   => Config\Constants::CAMPAIGN_METABOX_ATTACHMENTS,
+        'type' => 'file_list',
+        'text' => array(
+            'add_upload_files_text' => 'Adaugă materiale',
+            'remove_image_text' => 'Șterge material',
+            'file_text' => 'Material:',
+            'file_download_text' => 'Downloadează',
+            'remove_text' => 'Șterge',
+        ),
+    ));
+
+    $campaign_video_group_id = $cmb_campaign->add_field(array(
+        'id' => Config\Constants::CAMPAIGN_METABOX_VIDEO_GROUP,
+        'type' => 'group',
+        'description' => __('Listă materiale video (YouTube/Vimeo/etc.)', 'cmb2'),
+        'options'     => array(
+            'group_title'   => __('Video {#}', 'cmb2'), // since version 1.1.4, {#} gets replaced by row number
+            'add_button'    => __('Adaugă video', 'cmb2'),
+            'remove_button' => __('Șterge video', 'cmb2'),
+            'sortable'      => false, // beta
+            //'closed'     => true, // true to have the groups closed by default
+        ),
+    ));
+
+    $cmb_campaign->add_group_field(
+        $campaign_video_group_id,
+        array(
+            'name' => 'Titlu video',
+            'id'   => 'title',
+            'type' => 'text',
+            // 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+        )
+    );
+
+    $cmb_campaign->add_group_field(
+        $campaign_video_group_id,
+        array(
+            'name' => 'URL video',
+            'id'   => 'video_oembed',
+            'type' => 'oembed',
+            // 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+        )
+    );
+
+
+    // Custom Despre meta box
+    $cmb_parteneri = new_cmb2_box(array(
+        'id' => 'parteneri_despre',
+        'title' => __('Parteneri', 'cmb2'),
+        'object_types' => array('page'),
+        'show_on' => array(
+            'key' => 'slug',
+            'value' => 'despre',
+        ),
+        'context'       => 'advanced',
+    ));
+
+    $cmb_parteneri->add_field(array(
+        'name' => 'Descriere toți partenerii',
+        'id'   => Config\Constants::ABOUT_PAGE_METABOX_PARTNER_DESC,
+        'type' => 'wysiwyg',
+        'options' => array(
+            'wpautop' => true,
+            'media_buttons' => false,
+            'teeny' => true,
+        )
+    ));
+
+    $parteneri_group_id = $cmb_parteneri->add_field(array(
+        'id' => Config\Constants::ABOUT_PAGE_METABOX_PARTNERS,
+        'type' => 'group',
+        'description' => __('Secțiune parteneri', 'cmb2'),
+        'options'     => array(
+            'group_title'   => __('Partener {#}', 'cmb2'),
+            'add_button'    => __('Adaugă partener', 'cmb2'),
+            'remove_button' => __('Șterge partener', 'cmb2'),
+            'sortable'      => false,
+        ),
+    ));
+
+    $cmb_parteneri->add_group_field(
+        $parteneri_group_id,
+        array(
+            'name' => 'Logo partener',
+            'id'   => 'logo_partener',
+            'type' => 'file',
+            'options' => array(
+                'url' => false,
+            ),
+            'text' => array(
+                'add_upload_file_text' => 'Adaugă logo',
+            ),
+            'query_args' => array(
+                'type' => array(
+                    'image/gif',
+                    'image/jpeg',
+                    'image/png',
+                ),
+            ),
+            'preview_size' => 'medium'
+        )
+    );
+
+    $cmb_parteneri->add_group_field(
+        $parteneri_group_id,
+        array(
+            'name' => 'Descriere partener',
+            'id'   => 'descriere_partener',
+            'type' => 'wysiwyg',
+            'options' => array(
+                'wpautop' => true,
+                'media_buttons' => false,
+                'teeny' => true,
+            )
+        )
+    );
+});
+
+
+add_action('after_setup_theme', function () {
     /**
      * Enable features from Soil when plugin is activated
      * @link https://roots.io/plugins/soil/
