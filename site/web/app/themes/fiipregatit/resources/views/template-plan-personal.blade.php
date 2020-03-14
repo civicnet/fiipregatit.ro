67b73dd560afe4156bcdfd2a41c694a4b3e494aa
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-14 19:49:46 +0200

    fix(plan) show proper campaign count

--- site/web/app/themes/fiipregatit/resources/views/template-plan-personal.blade.php
+++ site/web/app/themes/fiipregatit/resources/views/template-plan-personal.blade.php
@@ -5,12 +5,17 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.jumbotron',
-  ['show_header' => false])
+  @include('partials.jumbotron', [
+    'show_header' => false
+  ])
 
-  @include('partials.content-plan-personal',
-  ['plan' => PlanPersonal::sections()])
+  @include('partials.content-plan-personal', [
+    'plan' => PlanPersonal::sections()
+  ])
 
-  @include('partials.campaign-section')
+  @include('partials.campaign-section', [
+    'show_count' => 3,
+    'show_button' => false
+  ])
   @include('partials.app-promo')
 @endsection
