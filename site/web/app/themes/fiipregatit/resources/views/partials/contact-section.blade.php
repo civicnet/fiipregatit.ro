a2463985afdde295180bb86b0015236ee0f4884f
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-18 23:48:57 +0200

    fix(contact) update styles

--- site/web/app/themes/fiipregatit/resources/views/partials/contact-section.blade.php
+++ site/web/app/themes/fiipregatit/resources/views/partials/contact-section.blade.php
@@ -10,8 +10,8 @@
           Numere de mai jos sunt <span style="color: #933">doar pentru informații și recomandări</span>
           despre COVID-19.
           <br/><br/>
-          <strong>România: <a href="tel:0800800358">0800 800 358</a></strong> (gratuit)<br/>
-          <strong>Diaspora: <a href="tel:0040213202020">+40 21 320 20 20</a></strong> (tarif normal în orice rețea)
+          <strong>România: <a href="tel:0800800358" class="bold">0800 800 358</a></strong> (gratuit)<br/>
+          <strong>Diaspora: <a href="tel:0040213202020" class="bold">+40 21 320 20 20</a></strong> (tarif normal în orice rețea)
           <br/><br/>
           Apelați <strong>112</strong> dacă aveți febră, tuse sau dificultăți în respirație și ați călătorit în
           țări cu cazuri confirmate de COVID-19 sau ați intrat în contact cu o persoană suspectă de infecție
@@ -23,18 +23,9 @@
       <div class="col-md-6 col-xs-12 contact-info-text ">
         <div style="width: 80%; margin: 0 auto;">
           <h3 style="margin-bottom: 30px; font-size: 1.55rem">Referitor la acest site</h3>
-          Adresă:
-          <a
-            href="https://maps.google.com?q=Piața+Revoluției+nr.1+A+sector+1+București"
-            rel="nofollow"
-            target="_blank">
-            Piața Revoluției, nr.1 A, sector 1, București
-          </a><br/>
-          Telefon <strong><a href="tel:021.312.25.47">021.312.25.47</a></strong><br/>
-          Centrala <a href="tel:021.303.70.80">021.303.70.80</a><br/><br/>
-
-          Fax: 021.313.71.55/021.264.86.46<br/>
-          E-mail: <strong><a href="mailto:dsu@mai.gov.ro">dsu@mai.gov.ro</a></strong><br/>
+          Telefon: <a href="tel:021.312.25.47">021.312.25.47</a><br/>
+          Centrala: <a href="tel:021.303.70.80">021.303.70.80</a><br/>
+          E-mail: <a href="mailto:dsu@mai.gov.ro">dsu@mai.gov.ro</a><br/>
           <div style="margin-top: 30px">
             {!! do_shortcode('[pirate_forms]') !!}
           </div>
