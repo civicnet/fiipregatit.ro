7c061e5ee7eceaa3393894794c00da4f9c1c4470
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-18 23:25:52 +0200

    fix(contact) add COVID-19 related contact info

--- site/web/app/themes/fiipregatit/resources/views/partials/contact-section.blade.php
+++ site/web/app/themes/fiipregatit/resources/views/partials/contact-section.blade.php
@@ -1,11 +1,28 @@
 <div class="container-fluid" id="contact-section">
   <div class="container contact-info">
     <h2>
-      VREI SĂ NE TRANSMIȚI UN MESAJ?
+      CONTACT
     </h2>
     <div class="row">
       <div class="contact-info-text col-md-6 col-xs-12">
-        <div>
+        <div style="width: 80%; margin: 0 auto">
+          <h3 style="margin-bottom: 30px; font-size: 1.55rem">Pentru COVID-19</h3>
+          Numere de mai jos sunt <span style="color: #933">doar pentru informații și recomandări</span>
+          despre COVID-19.
+          <br/><br/>
+          <strong>România: <a href="tel:0800800358">0800 800 358</a></strong> (gratuit)<br/>
+          <strong>Diaspora: <a href="tel:0040213202020">+40 21 320 20 20</a></strong> (tarif normal în orice rețea)
+          <br/><br/>
+          Apelați <strong>112</strong> dacă aveți febră, tuse sau dificultăți în respirație și ați călătorit în
+          țări cu cazuri confirmate de COVID-19 sau ați intrat în contact cu o persoană suspectă de infecție
+          cu noul coronavirus. <br/>
+          Specificați acest lucru dispecerului, evitați contactul cu alte persoane și <strong>NU vă deplasați
+          la spital/medic de familie!</strong>
+        </div>
+      </div>
+      <div class="col-md-6 col-xs-12 contact-info-text ">
+        <div style="width: 80%; margin: 0 auto;">
+          <h3 style="margin-bottom: 30px; font-size: 1.55rem">Referitor la acest site</h3>
           Adresă:
           <a
             href="https://maps.google.com?q=Piața+Revoluției+nr.1+A+sector+1+București"
@@ -18,40 +35,13 @@
 
           Fax: 021.313.71.55/021.264.86.46<br/>
           E-mail: <strong><a href="mailto:dsu@mai.gov.ro">dsu@mai.gov.ro</a></strong><br/>
+          <div style="margin-top: 30px">
+            {!! do_shortcode('[pirate_forms]') !!}
+          </div>
         </div>
       </div>
-      <div class="col-md-6 col-xs-12">
-        {!! do_shortcode('[pirate_forms]') !!}
-      </div>
     </div>
-  </div>
-  <h2>
-    Unde ne găsești
-  </h2>
-  <div class="row">
-    <div id="map"></div>
   </div>
 </div>
 
 
-<script>
-  function initMap() {
-    var marker_location = { lat: 44.4386238, lng: 26.0985546 };
-    // Styles a map in night mode.
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: marker_location,
-      zoom: 17,
-      styles: [{elementType:"geometry",stylers:[{color:"#242f3e"}]},{elementType:"labels.text.fill",stylers:[{color:"#746855"}]},{elementType:"labels.text.stroke",stylers:[{color:"#242f3e"}]},{featureType:"administrative.locality",elementType:"labels.text.fill",stylers:[{color:"#d59563"}]},{featureType:"poi",elementType:"labels.text.fill",stylers:[{color:"#d59563"}]},{featureType:"poi.park",elementType:"geometry",stylers:[{color:"#263c3f"}]},{featureType:"poi.park",elementType:"labels.text.fill",stylers:[{color:"#6b9a76"}]},{featureType:"road",elementType:"geometry",stylers:[{color:"#38414e"}]},{featureType:"road",elementType:"geometry.stroke",stylers:[{color:"#212a37"}]},{featureType:"road",elementType:"labels.text.fill",stylers:[{color:"#9ca5b3"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{color:"#746855"}]},{featureType:"road.highway",elementType:"geometry.stroke",stylers:[{color:"#1f2835"}]},{featureType:"road.highway",elementType:"labels.text.fill",stylers:[{color:"#f3d19c"}]},{featureType:"transit",elementType:"geometry",stylers:[{color:"#2f3948"}]},{featureType:"transit.station",elementType:"labels.text.fill",stylers:[{color:"#d59563"}]},{featureType:"water",elementType:"geometry",stylers:[{color:"#17263c"}]},{featureType:"water",elementType:"labels.text.fill",stylers:[{color:"#515c6d"}]},{featureType:"water",elementType:"labels.text.stroke",stylers:[{color:"#17263c"}]}]
-    });
-    var marker = new google.maps.Marker({
-      position: marker_location,
-      map: map
-    });
-  }
-</script>
-
-<script
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9EqyM2PGMOg7caE5rJlN1xJ2tTQm7HjQ&callback=initMap"
-  async
-  defer>
-</script>
