c253c451b4202ab8a3dba8b5e98301a17c43e9aa
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2020-03-19 00:32:55 +0200

    fix(search) add search bar to single guide, single campaign pages

--- site/web/app/themes/fiipregatit/resources/views/single-campanie.blade.php
+++ site/web/app/themes/fiipregatit/resources/views/single-campanie.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+  @include('partials.jumbotron',
+  ['show_header' => false])
+
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-single-'.get_post_type(), ['campaign' => Campanie::get()])
   @endwhile
